4faca5edf0acebd7057140eed31cc950a71e911e
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-06 07:17:02 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -8,8 +8,72 @@ let cryptoAPI = ('crypto' in self);
 
 let width;
 let height;
+const arrRootEntries = [];
 const mapFileKeys = new Map();
-let writableRoot;
+
+class EncryptedVolume {
+  constructor(hdlFolder, bufferKey) {
+    function getRootFile() {
+      return hdlVolumeFolder.getFileHandle("root.encrypt", {create: false});
+    }
+    function readRootFile(bufferKey) {
+      const fileRoot = fileHandleRoot.getFile();
+      const blobRootFile = readEncryptFile(bufferKey, fileRoot.arrayBuffer());
+      const bufferContents = blobRootFile.arrayBuffer();
+      const streamContents = new DataReadableStream(bufferContents);
+      arrRootEntries.clear();
+      while (!streamContents.atEnd()) {
+        try {
+          const record = {};
+          try {
+            record.dateValue = streamContents.getBigInt64(true);
+            record.numType = streamContents.getUint16(true);
+            const numLength = streamContents.getUint16(true);
+            record.bufferData = streamContents.getBuffer(numLength);
+          } catch (error) {
+            switch (error.type) {
+              case "RangeError":
+                throw new Error("Unexpected end of record", { cause: error });
+              default:
+                throw new Error("Unexpected error", { cause: error });
+            }
+          }
+          record.dateTimestamp = new Date(record.dateValue);
+          if (record.dateTimestamp.toValue() === NaN) {
+            throw new Error("Invalid date");
+          }
+          record.objData = interpretData(record.numType, record.bufferData);
+          delete record.dateValue;
+          delete record.bufferData;
+          Object.freeze(record);
+          arrRootEntries.push(record);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      function interpretData(numType, bufferData) {
+        const objRet = {};
+        const viewData = new DataView(bufferData);
+        switch (numType) {
+          case 0x1CD8:  // Remove file
+            if (bufferData.byteLength !== 4) {
+              throw new Error("Invalid record length");
+            }
+            objRet.numFileID = viewData.getUint32(0, true)
+            return objRet;
+          case 0x4BD8:  // New file (& key)
+            if (bufferData.byteLength !== 36) {
+              throw new Error("Invalid record length");
+            }
+            objRet.numFileID = viewData.getUint32(0, true)
+            objRet.bufferKey = bufferData.slice(4, 36);
+            return objRet;
+          default:
+            throw new Error("Unrecognized type");
+        }
+      }
+  }
+}
 
 window.addEventListener("load", function () {
   if (filesystemAPI) {
@@ -20,36 +84,50 @@ window.addEventListener("load", function () {
       btnMount.innerHTML = "Mount Volume";
       document.body.appendChild(btnMount);
       btnMount.addEventListener("click", function (evt) {
-        const promiseGetRoot = window.showDirectoryPicker().then(getRootFile).catch(alert);
-        const promiseReadRoot = Promise.all(promiseGetRoot, getMasterKey).then(readRootFile).catch(alert);
-
-        function getRootFile(entryParent) {
-          return entryParent.getFileHandle("root.encrypt", {create: false}).catch(handleError);
-          function handleError(error) {
-            function newRootFile(fileHandleRoot) {
-              writableRoot = fileHandleRoot.createWritable();
-              const contents = new ArrayBuffer(16);
-              contents[0] = 0x20;
-              contents[1] = 0x47;
-              contents[2] = 0x7D;
-              contents[3] = 0x46;
-              writableRoot.write(contents);
-            }
-            if (error.name === "NotFoundError") {
-              return entryParent.getFileHandle("root.encrypt", {create: true}).then(newRootFile).catch(alert);
-            }
+        function storeVolumeFolder(hdlFolder) {
+          hdlVolumeFolder = hdlFolder;
+        }
+        function getVolumeKey() {
+          return promptForString("Enter the volume password:", "").then(hash_SHA256);
+        }
+        }
+        const promiseGetVolumeFolder = window.showDirectoryPicker().then(storeVolumeFolder, handleGetVolumeFolderError);
+        const promiseGetVolumeKey = promiseGetVolumeFolder.then(getVolumeKey).catch(handleGetVolumeKeyError);
+        const promiseGetRootFile = promiseGetVolumeKey.then(getRootFile).then(readRootFile, handleGetRootFileError);
+        const promiseReadRootFile = promiseGetRootFile.then();
+        function handleGetVolumeFolderError(error) {
+          switch (error.type) {
+            case "Error":
+              throw new Error("User permission is required to access volume", { cause: error });
+            default:
+              throw new Error("Unexpected error", { cause: error });
           }
         }
-        function readRootFile([ fileHandleRoot, bufferKey ]) {
-          const fileRoot = fileHandleRoot.getFile();
-          const blobRootFile = readEncryptFile(bufferKey, fileRoot.arrayBuffer());
-          const bufferRootFile = blobRootFile.arrayBuffer();
-          const arrayRootFile = new DataView(bufferRootFile);
-          mapFileKeys.clear();
-          let numFileIndex = 0;
-          const numFileLength = bufferRootFile.byteLength;
-          while (numFileIndex < numFileLength) {
-//            mapFileKeys.add();
+        function handleGetVolumeKeyError(error) {
+          switch (error.type) {
+            case "Error":
+              throw new Error("Volume password is required to access volume", { cause: error });
+            default:
+              throw new Error("Unexpected error", { cause: error });
+          }
+        }
+        function handleGetRootFileError(error) {
+          switch (error.name) {
+            case "NotFoundError":
+              return hdlVolumeFolder.getFileHandle("root.encrypt", {create: true}).then(newRootFile);
+            default:
+              throw new Error("Unexpected error", { cause: error });
+          }
+          function newRootFile(fileHandleRoot) {
+            writableRoot = fileHandleRoot.createWritable();
+            const contents = new ArrayBuffer(16);
+            contents[0] = 0x20;
+            contents[1] = 0x47;
+            contents[2] = 0x7D;
+            contents[3] = 0x46;
+            const newVolumePassword = promptForString("No root file found.  Enter new volume password:", "").then(hash_SHA256);
+            const encryptedContents = writeEncryptFile(bufferKey, bufferData);
+            writableRoot.write(encryptedContents);
           }
         }
       });
@@ -63,14 +141,160 @@ window.addEventListener("load", function () {
   }
 });
 
+// Allows an ArrayBuffer to be accessed as a readable stream
+class DataReadableStream {
+  constructor(buffer, numStartIndex) {
+    if (numStartIndex) {
+      this._numIndex = numStartIndex;
+    } else {
+      this._numIndex = 0;
+    }
+    this._buffer = buffer;
+    this._view = new DataView(buffer);
+  }
+  seek(newIndex) {
+    this._numIndex = newIndex;
+  }
+  getIndex() {
+    return this._numIndex;
+  }
+  atEnd() {
+    return (this._numIndex >= this._buffer.byteLength);
+  }
+  getInt8() {
+    return this._view.getInt8(this._numIndex);
+    this._numIndex += 1;
+  }
+  getUint8() {
+    return this._view.getUint8(this._numIndex);
+    this._numIndex += 1;
+  }
+  getInt16(boolLittleEndian) {
+    return this._view.getInt16(this._numIndex, boolLittleEndian);
+    this._numIndex += 2;
+  }
+  getUint16(boolLittleEndian) {
+    return this._view.getUint16(this._numIndex, boolLittleEndian);
+    this._numIndex += 2;
+  }
+  getInt32(boolLittleEndian) {
+    return this._view.getInt32(this._numIndex, boolLittleEndian);
+    this._numIndex += 4;
+  }
+  getUint32(boolLittleEndian) {
+    return this._view.getUint32(this._numIndex, boolLittleEndian);
+    this._numIndex += 4;
+  }
+  getBigInt64(boolLittleEndian) {
+    return this._view.getInt64(this._numIndex, boolLittleEndian);
+    this._numIndex += 8;
+  }
+  getBigUint64(boolLittleEndian) {
+    return this._view.getUint64(this._numIndex, boolLittleEndian);
+    this._numIndex += 8;
+  }
+  getFloat32(boolLittleEndian) {
+    return this._view.getFloat32(this._numIndex, boolLittleEndian);
+    this._numIndex += 4;
+  }
+  getFloat64(boolLittleEndian) {
+    return this._view.getFloat64(this._numIndex, boolLittleEndian);
+    this._numIndex += 8;
+  }
+  getBuffer(numLength) {
+    return this._buffer.slice(this._numIndex, this._numIndex + numLength);
+    this._numIndex += numLength;
+  }
+}
+
+// Allows an ArrayBuffer to be accessed as a writable stream
+class DataWritableStream {
+  constructor(buffer, numStartIndex) {
+    if (numStartIndex) {
+      this._numIndex = numStartIndex;
+    } else {
+      this._numIndex = 0;
+    }
+    this._buffer = buffer;
+  }
+  seek(newIndex) {
+    this._numIndex = newIndex;
+  }
+  setIndex() {
+    return this._numIndex;
+  }
+  atEnd() {
+    return (this._numIndex >= this._buffer.byteLength);
+  }
+  setInt8(value) {
+    return this._buffer.getInt8(this._numIndex, value);
+    this._numIndex += 1;
+  }
+  setUint8(value) {
+    return this._buffer.getUint8(this._numIndex, value);
+    this._numIndex += 1;
+  }
+  setInt16(value, boolLittleEndian) {
+    return this._buffer.getInt16(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 2;
+  }
+  setUint16(value, boolLittleEndian) {
+    return this._buffer.getUint16(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 2;
+  }
+  setInt32(value, boolLittleEndian) {
+    return this._buffer.getInt32(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 4;
+  }
+  setUint32(value, boolLittleEndian) {
+    return this._buffer.getUint32(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 4;
+  }
+  setBigInt64(value, boolLittleEndian) {
+    return this._buffer.getInt64(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 8;
+  }
+  setBigUint64(value, boolLittleEndian) {
+    return this._buffer.getUint64(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 8;
+  }
+  setFloat32(value, boolLittleEndian) {
+    return this._buffer.getFloat32(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 4;
+  }
+  setFloat64(value, boolLittleEndian) {
+    return this._buffer.getFloat64(this._numIndex, value, boolLittleEndian);
+    this._numIndex += 8;
+  }
+  setBuffer(bufferInput) {
+    const arrThis = new Uint8Array(this._buffer);
+    const arrInput = new Uint8Array(bufferInput);
+    return arrThis.set(arrInput, this._numIndex);
+    this._numIndex += arrInput.length;
+  }
+}
+
+// If user clicks OK, promise resolves to the value entered by the user
+// If user clicks Cancel, promise rejects
+// strPrompt: (String) The prompt given to the user
+// strDefault: (String) The default response
+// Returns : (Promise, resolving to String)
+function promptForString(strPrompt, strDefault) {
+  return new Promise(function (resolve, reject) {
+    const strResponse = window.prompt(strPrompt, strDefault);
+    if (strResponse === null) {
+      resolve(strResponse);
+    } else {
+      reject(new Error("User declined to respond."));
+    }
+  });
+}
+
 // If not entered, prompts the user for the master password
 // If entered, returns the previously entered value
 // Returns : (Promise, resolving to ArrayBuffer)
 function getMasterKey() {
-  return new Promise(function (resolve, reject) {
-    const masterPassword = window.prompt("Enter the master password:");
-    resolve(hash_SHA256(masterPassword));
-  });
+  return promptForString("Enter the master password:", "").then(hash_SHA256(masterPassword));
 }
 
 // Produces random data of the requested size
