1b6e2a753f7bf7a7bdb1ad1a5a0b7cd2574515e8
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-07 18:10:56 -0400

    Create VolumeRootFile.js

---
+++ docs/VolumeRootFile.js
@@ -0,0 +1,20 @@
+class VolumeRootFile {
+  // Represents a volume root file
+  // blobFile: (Blob, optional) 
+  constructor(blobFile) {
+    this._records = [];
+  }
+  // Adds a record to the file
+  // numType: 
+  // objData: 
+  // Returns: (ArrayBuffer) The record as it will appear in the file
+  addRecord(numType, objData) {
+  }
+  // Remove unnecessary records from the file
+  purge() {
+  }
+  // Creates a Blob with the current records
+  // Returns: (Blob) Entire file containing all records
+  save() {
+  }
+}
