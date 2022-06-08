09cb87bf64ebc724334ea01e70094d5a7f539f0e
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-07 23:21:58 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -9,6 +9,12 @@ moduleVolumeRootFile.then(function (module) {
   console.log(Object.getOwnPropertyNames(module));
 }, console.error);
 
+let moduleEncryptedVolume = import("./EncryptedVolume.mjs");
+
+moduleEncryptedVolume.then(function (module) {
+  console.log(Object.getOwnPropertyNames(module));
+}, console.error);
+
 
 let filesystemAPI = ('showOpenFilePicker' in window && 'showDirectoryPicker' in window);
 let cryptoAPI = ('crypto' in self);
@@ -17,71 +23,6 @@ let width;
 let height;
 const arrRootEntries = [];
 const mapFileKeys = new Map();
-
-class EncryptedVolume {
-  constructor(hdlFolder, bufferKey) {
-    function getRootFile() {
-      return hdlFolder.getFileHandle("root.encrypt", {create: false});
-    }
-    function readRootFile(bufferKey) {
-      const fileRoot = fileHandleRoot.getFile();
-      const blobRootFile = readEncryptFile(bufferKey, fileRoot.arrayBuffer());
-      const bufferContents = blobRootFile.arrayBuffer();
-      const streamContents = new DataReadableStream(bufferContents);
-      arrRootEntries.clear();
-      while (!streamContents.atEnd()) {
-        try {
-          const record = {};
-          try {
-            record.dateValue = streamContents.getBigInt64(true);
-            record.numType = streamContents.getUint16(true);
-            const numLength = streamContents.getUint16(true);
-            record.bufferData = streamContents.getBuffer(numLength);
-          } catch (error) {
-            switch (error.type) {
-              case "RangeError":
-                throw new Error("Unexpected end of record", { cause: error });
-              default:
-                throw new Error("Unexpected error", { cause: error });
-            }
-          }
-          record.dateTimestamp = new Date(record.dateValue);
-          if (record.dateTimestamp.toValue() === NaN) {
-            throw new Error("Invalid date");
-          }
-          record.objData = interpretData(record.numType, record.bufferData);
-          delete record.dateValue;
-          delete record.bufferData;
-          Object.freeze(record);
-          arrRootEntries.push(record);
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      function interpretData(numType, bufferData) {
-        const objRet = {};
-        const viewData = new DataView(bufferData);
-        switch (numType) {
-          case 0x1CD8:  // Remove file
-            if (bufferData.byteLength !== 4) {
-              throw new Error("Invalid record length");
-            }
-            objRet.numFileID = viewData.getUint32(0, true)
-            return objRet;
-          case 0x4BD8:  // New file (& key)
-            if (bufferData.byteLength !== 36) {
-              throw new Error("Invalid record length");
-            }
-            objRet.numFileID = viewData.getUint32(0, true)
-            objRet.bufferKey = bufferData.slice(4, 36);
-            return objRet;
-          default:
-            throw new Error("Unrecognized type");
-        }
-      }
-    }
-  }
-}
 
 window.addEventListener("load", function () {
   if (filesystemAPI) {
@@ -148,138 +89,6 @@ window.addEventListener("load", function () {
   }
 });
 
-// Allows an ArrayBuffer to be accessed as a readable stream
-class DataReadableStream {
-  constructor(buffer, numStartIndex) {
-    if (numStartIndex) {
-      this._numIndex = numStartIndex;
-    } else {
-      this._numIndex = 0;
-    }
-    this._buffer = buffer;
-    this._view = new DataView(buffer);
-  }
-  seek(newIndex) {
-    this._numIndex = newIndex;
-  }
-  getIndex() {
-    return this._numIndex;
-  }
-  atEnd() {
-    return (this._numIndex >= this._buffer.byteLength);
-  }
-  getInt8() {
-    return this._view.getInt8(this._numIndex);
-    this._numIndex += 1;
-  }
-  getUint8() {
-    return this._view.getUint8(this._numIndex);
-    this._numIndex += 1;
-  }
-  getInt16(boolLittleEndian) {
-    return this._view.getInt16(this._numIndex, boolLittleEndian);
-    this._numIndex += 2;
-  }
-  getUint16(boolLittleEndian) {
-    return this._view.getUint16(this._numIndex, boolLittleEndian);
-    this._numIndex += 2;
-  }
-  getInt32(boolLittleEndian) {
-    return this._view.getInt32(this._numIndex, boolLittleEndian);
-    this._numIndex += 4;
-  }
-  getUint32(boolLittleEndian) {
-    return this._view.getUint32(this._numIndex, boolLittleEndian);
-    this._numIndex += 4;
-  }
-  getBigInt64(boolLittleEndian) {
-    return this._view.getInt64(this._numIndex, boolLittleEndian);
-    this._numIndex += 8;
-  }
-  getBigUint64(boolLittleEndian) {
-    return this._view.getUint64(this._numIndex, boolLittleEndian);
-    this._numIndex += 8;
-  }
-  getFloat32(boolLittleEndian) {
-    return this._view.getFloat32(this._numIndex, boolLittleEndian);
-    this._numIndex += 4;
-  }
-  getFloat64(boolLittleEndian) {
-    return this._view.getFloat64(this._numIndex, boolLittleEndian);
-    this._numIndex += 8;
-  }
-  getBuffer(numLength) {
-    return this._buffer.slice(this._numIndex, this._numIndex + numLength);
-    this._numIndex += numLength;
-  }
-}
-
-// Allows an ArrayBuffer to be accessed as a writable stream
-class DataWritableStream {
-  constructor(buffer, numStartIndex) {
-    if (numStartIndex) {
-      this._numIndex = numStartIndex;
-    } else {
-      this._numIndex = 0;
-    }
-    this._buffer = buffer;
-  }
-  seek(newIndex) {
-    this._numIndex = newIndex;
-  }
-  setIndex() {
-    return this._numIndex;
-  }
-  atEnd() {
-    return (this._numIndex >= this._buffer.byteLength);
-  }
-  setInt8(value) {
-    return this._buffer.getInt8(this._numIndex, value);
-    this._numIndex += 1;
-  }
-  setUint8(value) {
-    return this._buffer.getUint8(this._numIndex, value);
-    this._numIndex += 1;
-  }
-  setInt16(value, boolLittleEndian) {
-    return this._buffer.getInt16(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 2;
-  }
-  setUint16(value, boolLittleEndian) {
-    return this._buffer.getUint16(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 2;
-  }
-  setInt32(value, boolLittleEndian) {
-    return this._buffer.getInt32(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 4;
-  }
-  setUint32(value, boolLittleEndian) {
-    return this._buffer.getUint32(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 4;
-  }
-  setBigInt64(value, boolLittleEndian) {
-    return this._buffer.getInt64(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 8;
-  }
-  setBigUint64(value, boolLittleEndian) {
-    return this._buffer.getUint64(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 8;
-  }
-  setFloat32(value, boolLittleEndian) {
-    return this._buffer.getFloat32(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 4;
-  }
-  setFloat64(value, boolLittleEndian) {
-    return this._buffer.getFloat64(this._numIndex, value, boolLittleEndian);
-    this._numIndex += 8;
-  }
-  setBuffer(bufferInput) {
-    const arrThis = new Uint8Array(this._buffer);
-    const arrInput = new Uint8Array(bufferInput);
-    return arrThis.set(arrInput, this._numIndex);
-    this._numIndex += arrInput.length;
-  }
-}
 
 // If user clicks OK, promise resolves to the value entered by the user
 // If user clicks Cancel, promise rejects
